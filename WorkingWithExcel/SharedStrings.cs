2327b93544d61eb3ccfa9d9908f42a16fce46a17
M1KoTiK-Notebook <temhbz@mail.ru>
2023-12-09 02:33:09 +0700

    Начал делать логику парса SharedString

---
+++ WorkingWithExcel/SharedStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingWithExcel
+{
+    public class SharedStrings
+    {
+        public int Count { get; private set; }
+        public int UniqueCount { get; private set; }
+        public List<string> SharedString { get; private set; }
+        public SharedStrings(ExcelWorkspace workspace) 
+        {
+
+        }
+        
+    }
+}
--- WorkingWithExcel/Sheet.cs
+++ WorkingWithExcel/Sheet.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.Design.Serialization;
 using System.IO.Compression;
 using System.Linq;
 using System.Text;
@@ -11,75 +10,55 @@ namespace WorkingWithExcel
 {
     public class Sheet
     {
-        public string Name 
-        {
-            get
-            {
-                var nameAttr = xmlSheetElement.Attribute("name");
-                if (nameAttr == null) throw new NullReferenceException("При попытки парсить XmlSheet не найден атрибут name");
-                return nameAttr.Value;
-            }
-        }
-        public string SheetId 
-        { 
-            get 
-            {
-                var sheetIdAttr = xmlSheetElement.Attribute("sheetId");
-                if (sheetIdAttr == null) throw new NullReferenceException("При попытки парсить XmlSheet не найден атрибут sheetId");
-                return sheetIdAttr.Value;
-            } 
-        }
-        public string RId
-        {
-            get
-            {
-                var ns = xmlSheetElement.GetNamespaceOfPrefix("r");
-                var rIdAttr = xmlSheetElement.Attribute(ns + "id");
-                if (rIdAttr == null) throw new NullReferenceException("При попытки парсить XmlSheet не найден атрибут r:id");
-                return rIdAttr.Value;
-            }
-        }
+        public string Name { get; private set; }
+        public string SheetId { get; private set; }
+        public string RId { get; private set; }
+        public string SheetFileName { get; private set; }
 
-        public string Target 
-        { 
-            get 
-            {
-                using var fileStream = File.Open(workspace.ExcelFile.FullName, FileMode.Open);
-                using ZipArchive archive = new ZipArchive(fileStream, ZipArchiveMode.Update);
-                var relationshipArchive = archive.GetEntry("xl/_rels/workbook.xml.rels");
-                if (relationshipArchive == null) throw new NullReferenceException("Файл workbook.xml.rels отсутствует");
-                var stream = relationshipArchive.Open();
-                XDocument doc = XDocument.Load(stream);
 
-                var relationshipsElements = (from el in doc.Descendants() where el.Name.LocalName == "Relationships" select el).FirstOrDefault();
-                if (relationshipsElements == null) throw new NullReferenceException("при попытике спарсить workbook.xml.rels не найден атрибут Relationships");
-
-                var relationshipElement = (from el in relationshipsElements.Descendants() where el.Attribute("Id")?.Value == RId select el).FirstOrDefault();
-                if (relationshipElement == null) throw new NullReferenceException("При попытки парсить workbook.xml.rels не найден элемент соответствующий данной Sheet");
-
-                var targetAttr = relationshipElement.Attribute("Target");
-                if (targetAttr == null) throw new NullReferenceException("При попытки парсить workbook.xml.rels не найден атрибут Target");
-                stream.Dispose();
-                return targetAttr.Value;               
-            } 
-        }
-        private XElement relationship 
-        { 
-            get
-            { 
-
-            } 
-        }
-        private ExcelWorkspace workspace;
+        ExcelWorkspace workspace;
 
         private XElement xmlSheetElement;
-        public Sheet(ExcelWorkspace WS, XElement XMLSheetElement) 
-        {
-            this.xmlSheetElement= XMLSheetElement;
-            this.workspace = WS;                       
-        }
 
-        public WriteAtIndex()
+        public Sheet(ExcelWorkspace workspace, XElement XMLSheetElement) 
+        {
+            this.workspace = workspace;
+            this.xmlSheetElement = XMLSheetElement;
+
+            var nameAttr = xmlSheetElement.Attribute("name");
+            if (nameAttr == null) throw new NullReferenceException("При попытки парсить XmlSheet не найден атрибут name");
+            Name = nameAttr.Value;
+
+            var sheetIdAttr = xmlSheetElement.Attribute("sheetId");
+            if(sheetIdAttr == null) throw new NullReferenceException("При попытки парсить XmlSheet не найден атрибут sheetId");
+            SheetId = sheetIdAttr.Value;
+
+            var ns = xmlSheetElement.GetNamespaceOfPrefix("r");
+            var rIdAttr = xmlSheetElement.Attribute(ns + "id");
+            if (rIdAttr == null) throw new NullReferenceException("При попытки парсить XmlSheet не найден атрибут r:id");
+            RId = rIdAttr.Value;
+
+            using var fileStream = File.Open(workspace.ExcelFile.FullName, FileMode.Open);
+            using ZipArchive archive = new ZipArchive(fileStream, ZipArchiveMode.Update);
+            var relationshipArchive = archive.GetEntry("xl/_rels/workbook.xml.rels");
+            if (relationshipArchive == null) throw new NullReferenceException("Файл workbook.xml.rels отсутствует");
+            var stream = relationshipArchive.Open();
+            XDocument doc = XDocument.Load(stream);
+
+            var relationshipsElements = (from el in doc.Descendants() where el.Name.LocalName == "Relationships" select el).FirstOrDefault();
+            if( relationshipsElements == null) throw new NullReferenceException("при попытике спарсить workbook.xml.rels не найден атрибут Relationships");
+            stream.Dispose();
+
+            var relationshipElement = (from el  in relationshipsElements.Descendants() where el.Attribute("Id")?.Value == RId select el).FirstOrDefault();
+            if (relationshipElement == null) throw new NullReferenceException("При попытки парсить workbook.xml.rels не найден элемент соответствующий данной Sheet");
+
+            var targetAttr = relationshipElement.Attribute("Target");
+            if(targetAttr == null) throw new NullReferenceException("При попытки парсить workbook.xml.rels relationship (sheet) не найден атрибут Target");
+            SheetFileName = new FileInfo(targetAttr.Value).Name;
+
+
+        }
+        public void WriteValue(string value, int row, int col)
         {
 
         }
